f6a936ee87f9598dab3a40847a97f7e319aa9207
Marek Spiska <spiska.m@gmail.com>
2020-05-05 19:50:34 +0200

    fix(build): fixed heroku build

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/mo-ng'));
+app.use(express.static(__dirname + '/dist/cd-test'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/mo-ng/index.html'));
+res.sendFile(path.join(__dirname+'/dist/cd-test/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
